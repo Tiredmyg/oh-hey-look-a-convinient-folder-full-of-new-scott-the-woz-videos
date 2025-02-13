435e42a2cbae8498de186dd47d49d914422cd2a7
Mallory Gallegos <142539810+Tiredmyg@users.noreply.github.com>
2025-02-13 17:40:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function selectOption(option) {
         document.getElementById('no-button').innerText = 'bruh';
         //increase text size of yes
         var yesButton = document.getElementById('yes-button');
-        var currentFontSize = window.getComputedStyle(yes - button).getPropertyValue('font-size');
+        var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
         var newSize = parseFloat(currentFontSize) * 2; //increase font size by * 2px
         yesButton.style.fontSize = newSize + 'px';
 
@@ -44,7 +44,7 @@ function flashRainbowColors(callback) {
 }
 
 //function to display the maxwell gif initially
-function displaycat.gif() {
+function displaycat() {
     // container where image is displayed 
     var imageContainer = document.getElementById('image-container');
     // new image element for kibby
@@ -55,12 +55,12 @@ function displaycat.gif() {
     catImage.alt = 'Cat';
     //when cat image is fully loaded add it to image container 
     catImage.onload = function () {
-        imagecontainer.appendChild(catImage);
+        imageContainer.appendChild(catImage);
     };
 }
 
 //function to display heart yippie gif me thinks 
-function displayyippie.gif() {
+function displayyippie() {
     //clear  exisiting content in the image container
     document.getElementById('image-container').innerHTML = '';
     //get the container where the image will be displayed
@@ -75,7 +75,7 @@ function displayyippie.gif() {
     yippieImage.onload = function () {
         imageContainer.appendChild(yippieImage);
         //hide options container
-        document.getElementById('options'), style.display = 'none';
+        document.getElementById('options').style.display = 'none';
     };
 }
 
